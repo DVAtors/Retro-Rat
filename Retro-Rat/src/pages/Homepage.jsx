c8c719c252567eb8b8b69cb28b701769cd09f96e
Robert van Eijk <251185@virtualwindow.co.za>
2026-05-28 09:57:19 +0200

    file name changes
    renamed homepage files so that it would load

---
+++ Retro-Rat/src/pages/Homepage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Homepage.css";
+
+import bootRatWhite from "../assets/bootRatWhite.svg";
+
+export default function Home() {
+	return (
+		<>
+			<div className="home-container">
+				<div className="home-content">
+					<Container fluid className="hero-section">
+						<Row className="hero-container">
+							<Col>
+								<img src={bootRatWhite} alt="Boot Rat" className="hero-image" />
+							</Col>
+
+							<Col className="hero-text-container">
+								<div className="hero-text">
+									<h1>Vintage Market Place</h1>
+									<p>
+										Buy and sell authentic retro electronics from verified
+										collectors
+									</p>
+								</div>
+
+								<button className="browse-button">Browse Now!</button>
+							</Col>
+						</Row>
+					</Container>
+				</div>
+			</div>
+		</>
+	);
+}
