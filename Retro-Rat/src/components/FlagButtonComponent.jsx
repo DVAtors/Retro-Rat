53581bde3e020eb67568a8e0712d72705616b708
Robert van Eijk <251185@virtualwindow.co.za>
2026-05-29 14:37:09 +0200

    updated edit button on single product view page.
    Edit button, FlagButtoncomponent is now a pencil from fontawesome

--- Retro-Rat/src/components/FlagButtonComponent.jsx
+++ Retro-Rat/src/components/FlagButtonComponent.jsx
@@ -1,32 +1,13 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 
 function FlagButtonComponent({ listingId }) {
-  return (
-    <Link to={`/sell/${listingId}`} className="flagIconBtn">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="25"
-        height="25"
-        viewBox="0 0 25 25"
-        fill="none">
-        <path
-          d="M4.16602 15.6247C4.16602 15.6247 5.20768 14.583 8.33268 14.583C11.4577 14.583 13.541 16.6663 16.666 16.6663C19.791 16.6663 20.8327 15.6247 20.8327 15.6247V3.12467C20.8327 3.12467 19.791 4.16634 16.666 4.16634C13.541 4.16634 11.4577 2.08301 8.33268 2.08301C5.20768 2.08301 4.16602 3.12467 4.16602 3.12467V15.6247Z"
-          fill="#ffffff"
-          stroke="black"
-          stroke-width="2.08333"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M4.16602 22.9167V15.625"
-          stroke="black"
-          stroke-width="2.08333"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    </Link>
-  );
+	return (
+		<Link to={`/sell/${listingId}`} className="flagIconBtn">
+			<FontAwesomeIcon icon={faPencil} />
+		</Link>
+	);
 }
 
 export default FlagButtonComponent;
